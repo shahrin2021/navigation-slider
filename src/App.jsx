6a221021c6dea5210f9f108f7e848b9e1c2ba183
Md Shahrin Bin Selim <mdshahrinbinselim1991@gmail.com>
2021-11-23 13:52:57 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+import "./styles.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  EffectFade,Navigation,Pagination
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([EffectFade,Navigation,Pagination]);
+
+
+export default function App() {
+  
+  
+  
+  return (
+    <>
+    <Swiper spaceBetween={30} effect={'fade'} navigation={true} pagination={{
+  "clickable": true
+}} className="mySwiper">
+  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide>
+  </Swiper>
+    </>
+  )
+}
